6b44af01dba8da7df7bafd16137417b811dea834
Mega Farts <clint.m.thompson@outlook.com>
2021-05-07 15:22:55 -0500

    Created a seperate chart for searching by DOB

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function searchByName(){
     if(filteredPeople.length > 0){
         return(filteredPeople);
     }else{
-        alert('Sorry, looks like there is no one with that name.');
+        alert('Sorry, looks like there is no one with that name. Please try again.');
     }
 }
 
@@ -59,3 +59,59 @@ newTable.map(function(el){
 })
 }
 
+
+function searchByDob(){
+    
+    let dobInput = document.forms['dobForm']['dob'].value;
+
+    
+    let filteredPeople = people.filter(function (person) {
+        if(person.dob === dobInput){
+            return true;
+        }
+        return false;
+    });
+    
+    
+    if(filteredPeople.length > 0){
+        return(filteredPeople);
+    }else{
+        alert('Sorry, looks like there is no one with that birthday. Please try again.');
+    }
+}
+
+let birthdayTable = people.map(function(el){
+    document.getElementById("birthdayTable").innerHTML += `<tr>
+        <td>${el.id}</td>
+        <td>${el.firstName}</td>
+        <td>${el.lastName}</td>
+        <td>${el.gender}</td>
+        <td>${el.dob}</td>
+        <td>${el.height}</td>
+        <td>${el.weight}</td>
+        <td>${el.eyeColor}</td>
+        <td>${el.occupation}</td>
+        <td>${el.parents}</td>
+        <td>${el.currentSpouse}</td>
+    </tr>`
+})
+
+function getNewBirthdayTable(){
+    let newBirthdayTable= searchByDob();
+    document.getElementById("birthdayTable").innerHTML = ''
+newBirthdayTable.map(function(el){
+    document.getElementById("birthdayTable").innerHTML += `<tr>
+        <td>${el.id}</td>
+        <td>${el.firstName}</td>
+        <td>${el.lastName}</td>
+        <td>${el.gender}</td>
+        <td>${el.dob}</td>
+        <td>${el.height}</td>
+        <td>${el.weight}</td>
+        <td>${el.eyeColor}</td>
+        <td>${el.occupation}</td>
+        <td>${el.parents}</td>
+        <td>${el.currentSpouse}</td>
+    </tr>`
+})
+}

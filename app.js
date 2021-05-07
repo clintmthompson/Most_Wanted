dd67c8f74c7869c57d60a54b110297376f459f54
Mega Farts <clint.m.thompson@outlook.com>
2021-05-07 13:05:08 -0500

    Started a function to build a new table based on search input

--- app.js
+++ app.js
@@ -7,21 +7,22 @@ function searchByName(){
 
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     let filteredPeople = people.filter(function (person) {
-        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
+        if(person.firstName === firstNameInput || person.lastName === lastNameInput){
             return true;
+            console.log(filteredPeople);
         }
         return false;
     });
     
     // Rather than console logging, you need to append the filteredPeople to a table.
     if(filteredPeople.length > 0){
-        console.log(filteredPeople);
+        return(filteredPeople);
     }else{
         console.log('Sorry, looks like there is no one with that name.');
     }
 }
 
-people.map(function(el){
+let firstTable = people.map(function(el){
     document.getElementById("data").innerHTML += `<tr>
         <td>${el.id}</td>
         <td>${el.firstName}</td>
@@ -38,40 +39,25 @@ people.map(function(el){
 })
 
 
-//Storing specific contents of array in new variables by type
+let newTable = searchByName()
 
-let listIds = people.map(function(el){
-    return(el.id)
+function getNewTable(){
+newTable.map(function(el){
+    document.getElementById("data").innerHTML += `<tr>
+        <td>${el.id}</td>
+        <td>${el.firstName}</td>
+        <td>${el.lastName}</td>
+        <td>${el.gender}</td>
+        <td>${el.dob}</td>
+        <td>${el.height}</td>
+        <td>${el.weight}</td>
+        <td>${el.eyeColor}</td>
+        <td>${el.occupation}</td>
+        <td>${el.parents}</td>
+        <td>${el.currentSpouse}</td>
+    </tr>`
 })
-let lisFirstName = people.map(function(el){
-    return(el.firstName)
-})
-let listLastName = people.map(function(el){
-    return(el.lastName)
-})
-let listGender = people.map(function(el){
-    return(el.gender)
-})
-let listDob = people.map(function(el){
-    return(el.dob)
-})
-let listHeight = people.map(function(el){
-    return(el.height)
-})
-let listWeight = people.map(function(el){
-    return(el.weight)
-})
-let listEyeColor = people.map(function(el){
-    return(el.eyeColor)
-})
-let listOccupation = people.map(function(el){
-    return(el.occupation)
-})
-let listParents = people.map(function(el){
-    return(el.parents)
-})
-let listCurrentSpouse = people.map(function(el){
-    return(el.currentSpouse)
-})
+}
 
-console.log(listOccupation[0]) //testing calling a specific index from stored array
+
+
